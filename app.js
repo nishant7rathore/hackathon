26c67935afa296af400217663942ace91a076349
Nishant <nishant7rathore@gmail.com>
2024-11-29 18:05:21 -0500

    Express app bootstrap

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 const port = 3000;
+const __dirname = path.resolve(path.dirname('')); 
+
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
 
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);  
+    console.log(`Server listening on port ${port}`);  
+  });
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/index.html'))
 });
 
-app.get('/', (req, res) => {
-    res.send('Hello World');  
-  });
+
