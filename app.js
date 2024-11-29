5909beb33b2fa4b54148b525043648b5c616d593
Nishant <nishant7rathore@gmail.com>
2024-11-29 18:20:42 -0500

    added bootstrap stuff

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import { resolve, dirname, join } from 'path';
 const app = express();
 
 const port = 3000;
-const __dirname = path.resolve(path.dirname('')); 
+const __dirname = resolve(dirname('')); 
 
-app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
-app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
-app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);  
   });
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, 'views/index.html'))
+  res.sendFile(join(__dirname, 'views/index.html'))
 });
 
 
