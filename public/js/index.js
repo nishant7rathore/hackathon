d125e7fc515fcc287e3291c7bc536b11eced8ee1
Nishant <nishant7rathore@gmail.com>
2024-11-30 23:20:55 -0500

    pushed more changes frontend

--- public/js/index.js
+++ public/js/index.js
@@ -9,18 +9,11 @@ $(function() {
         $( "#nameText2" ).text( username+'\'s' );
     }
 
-    console.log(localStorage.getItem("username"));
-
         // Check browser support
-    if (typeof(Storage) !== "undefined" && localStorage.getItem("username") != null) {
+    if (typeof(Storage) !== "undefined" && localStorage.getItem("username") != null && localStorage.getItem("username") !== 'undefined') {
         // Store
+        console.log(localStorage.getItem("username"));
         localStorage.setItem("username", username);
         localStorage.setItem("password", password);
-    }
-    else {
-        const username = localStorage.getItem("username");
-        const password = localStorage.getItem("password");
-        $( "#nameText1" ).text( username+'\'s' );
-        $( "#nameText2" ).text( username+'\'s' );
     }
     });
